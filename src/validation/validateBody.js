55a4fe2ea0016afa5ce7ba2006affe78ec71f8d5
Neon <neonmed571@gmail.com>
2025-05-30 16:53:41 +0300

    fix bugs with crud

--- src/validation/validateBody.js
+++ src/validation/validateBody.js
@@ -3,7 +3,7 @@ import createHttpError from 'http-errors';
 export const validateBody = (schema) => {
   return async (req, res, next) => {
     try {
-      await schema.valiadteAsync(req.body, { abortEarly: false });
+      await schema.validateAsync(req.body, { abortEarly: false });
       next();
     } catch (err) {
       const error = createHttpError(400, 'Bad Request!', {
