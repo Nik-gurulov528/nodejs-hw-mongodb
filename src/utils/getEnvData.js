b233b451b72286d6ec21f1d054413db1272083db
Neon <neonmed571@gmail.com>
2025-06-17 23:28:09 +0300

    remake get env data function

--- src/utils/getEnvData.js
+++ src/utils/getEnvData.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const getEnvData = (name, defaultValue) => {
+export const getEnvData = async (name, defaultValue) => {
   const value = process.env[name];
 
   if (value) {
