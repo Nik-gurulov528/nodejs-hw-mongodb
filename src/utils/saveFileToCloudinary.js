1662359e66e57a04b8b7f1654e968a557c15542c
Neon <neonmed571@gmail.com>
2025-06-17 23:55:08 +0300

    change env data

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -2,16 +2,12 @@ import cloudinary from 'cloudinary';
 import * as fs from 'node:fs/promises';
 import { getEnvData } from './getEnvData.js';
 
-const initCloudinarySettings = async () => {
-  cloudinary.v2.config({
-    secure: true,
-    cloud_name: await getEnvData('CLOUD_NAME'),
-    api_key: await getEnvData('API_KEY'),
-    api_secret: await getEnvData('API_SECRET'),
-  });
-};
-
-await initCloudinarySettings();
+cloudinary.v2.config({
+  secure: true,
+  cloud_name: await getEnvData('CLOUD_NAME'),
+  api_key: await getEnvData('API_KEY'),
+  api_secret: await getEnvData('API_SECRET'),
+});
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,14 @@
 import nodemailer from 'nodemailer';
 import { getEnvData } from './getEnvData.js';
 
-const makeTransporter = async () => {
-  const transporter = nodemailer.createTransport({
-    host: await getEnvData('SMTP_HOST'),
-    port: await getEnvData('SMTP_PORT'),
-    auth: {
-      user: await getEnvData('SMTP_USER'),
-      pass: await getEnvData('SMTP_PASSWORD'),
-    },
-  });
-
-  return transporter;
-};
-
-const transporter = await makeTransporter();
+const transporter = nodemailer.createTransport({
+  host: await getEnvData('SMTP_HOST'),
+  port: await getEnvData('SMTP_PORT'),
+  auth: {
+    user: await getEnvData('SMTP_USER'),
+    pass: await getEnvData('SMTP_PASSWORD'),
+  },
+});
 
 export const sendEmail = async (options) => {
   return await transporter.sendMail(options);
