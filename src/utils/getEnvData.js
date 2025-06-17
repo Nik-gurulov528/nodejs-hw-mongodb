3b3790e21a6c564f64cee9f232153bf0bb23851d
Neon <neonmed571@gmail.com>
2025-06-18 00:29:04 +0300

    normalize code

--- src/utils/getEnvData.js
+++ src/utils/getEnvData.js
@@ -4,9 +4,6 @@ dotenv.config();
 
 export const getEnvData = (name, defaultValue) => {
   const value = process.env[name];
-  if (name === 'CLOUD_NAME') {
-    console.log(value);
-  }
 
   if (value) {
     return value;
