42f22ffa549ed533b38c243d0a80036045d63df7
Neon <neonmed571@gmail.com>
2025-06-17 23:49:04 +0300

    remake send email function

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,14 +1,20 @@
 import nodemailer from 'nodemailer';
 import { getEnvData } from './getEnvData.js';
 
-const transporter = nodemailer.createTransport({
-  host: await getEnvData('SMTP_HOST'),
-  port: await getEnvData('SMTP_PORT'),
-  auth: {
-    user: await getEnvData('SMTP_USER'),
-    pass: await getEnvData('SMTP_PASSWORD'),
-  },
-});
+const makeTransporter = async () => {
+  const transporter = nodemailer.createTransport({
+    host: await getEnvData('SMTP_HOST'),
+    port: await getEnvData('SMTP_PORT'),
+    auth: {
+      user: await getEnvData('SMTP_USER'),
+      pass: await getEnvData('SMTP_PASSWORD'),
+    },
+  });
+
+  return transporter;
+};
+
+const transporter = await makeTransporter();
 
 export const sendEmail = async (options) => {
   return await transporter.sendMail(options);
