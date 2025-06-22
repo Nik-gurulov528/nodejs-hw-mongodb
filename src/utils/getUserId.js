9807b6012eca53dc4bcdfb945810b1088a51937d
Neon <neonmed571@gmail.com>
2025-06-22 14:34:09 +0300

    modify user get id

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,6 +1,14 @@
-export const getUserId = (request) => {
-  const cookiesUrl = request.headers.cookie;
-  const userIdUrl = cookiesUrl.slice(cookiesUrl.indexOf(' '));
-  const userId = userIdUrl.slice(userIdUrl.indexOf('=') + 1);
+// export const getUserId = (request) => {
+//   const cookiesUrl = request.headers.cookie;
+//   const userIdUrl = cookiesUrl.slice(cookiesUrl.indexOf(' '));
+//   const userId = userIdUrl.slice(userIdUrl.indexOf('=') + 1);
+//   return userId;
+// };
+
+export const getUserId = (req) => {
+  const userId = req.cookies?.userId;
+  if (!userId) {
+    throw new Error('Missing userId cookie');
+  }
   return userId;
 };
